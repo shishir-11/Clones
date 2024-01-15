f04d4bbf88bc157a7d2b62517011ea136fe9fcf9
Shishir <kushwahashishir1112@gmail.com>
2024-01-15 15:57:27 +0530

    Added video preview system on Hover

--- src/Components/list/List.jsx
+++ src/Components/list/List.jsx
@@ -29,18 +29,17 @@ const List = () => {
             <div className='wrapper'>
                 <ArrowBackIosIcon className='sliderArrow left' onClick={()=>{handleClick("left")}} style={{visibility:`${slideNumber>0?"":"hidden"}`}}/>
                 <div className='container' ref={cont_ref}>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-
+                    <ListItem index={0}/>
+                    <ListItem index={1}/>
+                    <ListItem index={2}/>
+                    <ListItem index={3}/>
+                    <ListItem index={4}/>
+                    <ListItem index={5}/>
+                    <ListItem index={6}/>
+                    <ListItem index={7}/>
+                    <ListItem index={8}/>
+                    <ListItem index={9}/>
+                    <ListItem index={10}/>
 
 
                 </div>
--- src/Components/listItem/ListItem.jsx
+++ src/Components/listItem/ListItem.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ListItem.css';
+import show from './download.jpg';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-const ListItem = () => {
+const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
+const ListItem = (index) => {
+    const [isHovered,setIsHovered] = useState(false);
+
+
     return (
-        <div className='listItem'>
+        <div className='listItem' 
+            onMouseEnter={()=>setIsHovered(true)}
+            onMouseLeave={()=>setIsHovered(false)}
+            style={{left: isHovered && index*225 -50}}
+        >
+        {!isHovered && <img src={show} alt=""/>}
+
+        {isHovered && (
+            <>
+            <video src={trailer} autoPlay loop controls/>
+            <div className='itemInfo'>
+                <div className='icons'>
+                    <PlayArrowIcon className='icon'/>
+                    <AddIcon className='icon'/>
+                    <FavoriteBorderIcon className='icon'/>
+                </div>
+            <div className='itemInfoTop'>
+                <span>1 Hours 14 min</span>
+                <span className='limit'>+16</span>
+                <span>1999</span>
+            </div>
+            <div className='desc'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum et eu orci eget semper.
+                Phasellus sodales viverra porta.
+            </div>
+            <div className='genre'>Action</div>
+            </div>
+
+            </>
+        )}
+        
         </div>
     );
 }
